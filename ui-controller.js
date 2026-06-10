6c7770bb267fa8e814816e3964e43617c49eef44
felipedavi0103-png <felipedavi0103@gmail.com>
2026-06-10 14:41:46 -0300

    Update ui-controller.js

--- ui-controller.js
+++ ui-controller.js
@@ -1,6 +1,17 @@
-window.toggleAppMode = function () {
-    const isApp = localStorage.getItem("app-mode") === "1";
+// Dentro do seu arquivo sw.js:
+const CACHE_NAME = 'lampfinance-v2'; // <--- Mude o número da versão aqui sempre que atualizar o design
 
-    localStorage.setItem("app-mode", isApp ? "0" : "1");
-    location.reload(); // aplica em todas as páginas
-};
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Deletando cache antigo...');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Força o novo design a assumir o controle na hora
+    );
+});
